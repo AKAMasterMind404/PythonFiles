01ae33971eb5f385201c150a7000e07a77c2a074
AKAMasterMind404 <atharvkarbhari123@gmail.com>
2021-06-13 01:40:42 +0530

    newCommits

--- aRough4.py
+++ aRough4.py
@@ -3,12 +3,18 @@ arr = list(map(int, input().split()))
 d = {}
 for i in range(len(arr)):
     if d.get(arr[i]):
-        d[arr[i]].append(i+1)
+        d[arr[i]].append(i + 1)
     else:
-        d[arr[i]]=[i+1]
+        d[arr[i]] = [i + 1]
 
 q = int(input())
 for i in range(q):
     num = int(input())
-
-print(d)
+    if d.get(num):
+        print("Yes", d.get(num)[0])
+    else:
+        for j, k in enumerate(arr):
+            if k > num:
+                print("No", j+1)
+                break
+# print(d)
