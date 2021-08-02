4b467bb3c8d19a6b13b294903a24464a174f06b3
AKAMasterMind404 <atharvkarbhari123@gmail.com>
2021-08-03 00:11:21 +0530

    .

--- aRough2.py
+++ aRough2.py
@@ -1,4 +1,4 @@
 with open("aRough4.py","r") as file:
-    states = file.readline().split(',')
-    for s in states:
-        print("\""+s.strip()+"\"", end=",")
+    for s in file:
+        name = s.strip()
+        print(f"\"{name}\": coin.{name},")
--- aRough4.py
+++ aRough4.py
@@ -1,1 +1,26 @@
-"Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chhattisgarh", "Goa", "Gujarat", "Haryana", "Himachal Pradesh", "Jharkhand", "Karnataka", "Kerala", "Madhya Pradesh", "Maharashtra", "Manipur", "Meghalaya", "Mizoram", "Nagaland", "Odisha", "Punjab", "Rajasthan", "Sikkim", "Tamil Nadu", "Telangana", "Tripura", "Uttar Pradesh", "Uttarakhand", "Dehradun", "West Bengal",
+ath
+ath_change_percentage
+ath_date
+atl
+atl_change_percentage
+atl_date
+circulating_supply
+current_price
+fully_diluted_valuation
+high_24h
+id
+image
+last_updated
+low_24h
+market_cap
+market_cap_change_24h
+market_cap_change_percentage_24h
+market_cap_rank
+max_supply
+name
+price_change_24h
+price_change_percentage_24h
+price_change_percentage_24h_in_currency
+symbol
+total_supply
+total_volume
