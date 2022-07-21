9c1af4f9a72f07668678b674f19d34094faa4442
Atharv Karbhari <59548859+AKAMasterMind404@users.noreply.github.com>
2022-07-21 15:57:42 +0530

    21/7/2022

--- aRough1.py
+++ aRough1.py
@@ -1,1 +1,10 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
 
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+
+
+n = 10
+s = Solution()
+s.firstBadVersion()
